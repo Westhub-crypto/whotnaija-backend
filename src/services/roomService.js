71cc0031d13ea7d775f5d598cbe24ce9e370b047
Westhub-crypto <godwinoloja4@gmail.com>
2026-07-08 10:51:22 +0100

    Implement room cleanup service for abandoned rooms
    This service checks for abandoned game rooms and updates their status, as well as the online status of players.

---
+++ src/services/roomService.js
@@ -0,0 +1,24 @@
+// roomService.js
+const GameRoom = require('../models/GameRoom');
+const User = require('../models/User');
+const logger = require('./logger');
+
+async function cleanupAbandonedRooms() {
+  const cutoff = new Date(Date.now() - 10 * 60 * 1000); // 10 min ago
+  const abandonedRooms = await GameRoom.find({
+    status: 'in-progress',
+    'timer.endsAt': { $lt: new Date() },
+  });
+
+  for (const room of abandonedRooms) {
+    await GameRoom.findByIdAndUpdate(room._id, { status: 'abandoned' });
+    for (const player of room.players) {
+      if (!player.isBot && player.userId) {
+        await User.findByIdAndUpdate(player.userId, { onlineStatus: 'online' });
+      }
+    }
+    logger.info(`Cleaned up abandoned room: ${room.roomId}`);
+  }
+}
+
+module.exports = { cleanupAbandonedRooms };
